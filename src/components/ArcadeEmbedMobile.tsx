0cb411599ebd8c471de336d44fb334bc1a4794e3
Francisco Forte <aigenius.xn@gmail.com>
2025-09-23 09:55:06 +0100

    Create ArcadeEmbedMobile.tsx

---
+++ src/components/ArcadeEmbedMobile.tsx
@@ -0,0 +1,16 @@
+export function ArcadeEmbedMobile() {
+    return (
+      <div style={{ position: 'relative', paddingBottom: 'calc(153.55555555555554% + 41px)', height: 0, width: '100%' }}>
+        <iframe
+          src="https://demo.arcade.software/fFMkm7oqMrwPW0cwJl5w?embed&embed_mobile=inline&embed_desktop=inline&show_copy_link=true"
+          title="Generate User-Generated Content Images with AI"
+          frameBorder="0"
+          loading="lazy"
+          allowFullScreen
+          allow="clipboard-write"
+          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', colorScheme: 'light' }}
+        />
+      </div>
+    )
+  }
+  
